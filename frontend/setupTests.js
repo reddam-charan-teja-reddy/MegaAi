b23ba4804745ff4c5eeda4b889f16ed0f712f887
charan <c9014028307@outlook.com>
2026-05-09 19:20:22 +0530

    feat: add canvas context stubs for JSDOM in setupTests.js

--- frontend/setupTests.js
+++ frontend/setupTests.js
@@ -1,1 +1,25 @@
 import '@testing-library/jest-dom';
+
+// JSDOM does not implement canvas, so stub minimal APIs used in tests.
+HTMLCanvasElement.prototype.getContext = () => ({
+	drawImage: () => {},
+	clearRect: () => {},
+	getImageData: () => ({ data: [] }),
+	putImageData: () => {},
+	createImageData: () => [],
+	setTransform: () => {},
+	save: () => {},
+	restore: () => {},
+	beginPath: () => {},
+	moveTo: () => {},
+	lineTo: () => {},
+	closePath: () => {},
+	stroke: () => {},
+	translate: () => {},
+	scale: () => {},
+	rotate: () => {},
+	arc: () => {},
+	fill: () => {},
+	fillRect: () => {},
+	strokeRect: () => {},
+});
